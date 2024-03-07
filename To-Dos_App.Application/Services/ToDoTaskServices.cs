0e1dbf472c2577417db63e3b6ccf917005369527
kenny.villalobos.kuco@gmail.com <kenny.villalobos.kuco@gmail.com>
2024-03-07 18:43:04 -0500

    Adding Application Services

--- To-Dos_App.Application/Services/ToDoTaskServices.cs
+++ To-Dos_App.Application/Services/ToDoTaskServices.cs
@@ -3,10 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using To_Dos_App.Core;
+using To_Dos_App.Core.Entities;
+using To_Dos_App.Core.Interfaces;
+using To_Dos_App.Core.Results;
+using To_Dos_App.Infraestructure.Repositories;
 
 namespace To_Dos_App.Application.Services
 {
-    public class ToDoTaskServices
+    public class ToDoTaskServices : IToDoTaskServices
     {
+        private readonly ToDoTaskRepository _taskRepo;
+        public ToDoTaskServices(ToDoTaskRepository taskRepo)
+        {
+            _taskRepo = taskRepo;
+        }
+
+        public async Task<Result<bool, Error>> AddToDoTask(ToDoTask task)
+        {
+            var result = await _taskRepo.AddToDoTaskAsync(task);
+            return result;
+        }
+
+        public async Task<Result<bool, Error>> Delete(Guid Id)
+        {
+            var result = await _taskRepo.DeleteTask(Id);
+            return result;
+        }
+
+        public Task<Result<bool, Error>> EditTaskMessage(Guid Id, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<ToDoTask>, Error>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<ToDoTask>, Error>> GetAllCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<ToDoTask>, Error>> GetAllCompleted(string substring)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<ToDoTask>, Error>> GetAllIncompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<ToDoTask>, Error>> GetAllIncompleted(string substring)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<bool, Error>> MarkCompleteTask(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ To-Dos_App.Core/Interfaces/IToDoTaskServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using To_Dos_App.Core.Entities;
+using To_Dos_App.Core.Results;
+
+namespace To_Dos_App.Core.Interfaces
+{
+    public interface IToDoTaskServices
+    {
+        Task<Result<bool, Error>> AddToDoTask(ToDoTask task);
+        Task<Result<bool, Error>> MarkCompleteTask(Guid Id);
+        Task<Result<bool, Error>> EditTaskMessage(Guid Id, string message);
+        Task<Result<List<ToDoTask>, Error>> GetAll();
+        Task<Result<bool, Error>> Delete(Guid Id);
+        Task<Result<List<ToDoTask>, Error>> GetAllCompleted();
+        Task<Result<List<ToDoTask>, Error>> GetAllIncompleted();
+        Task<Result<List<ToDoTask>, Error>> GetAllCompleted(string substring);
+        Task<Result<List<ToDoTask>, Error>> GetAllIncompleted(string substring);
+
+
+
+
+
+
+
+    }
+}
